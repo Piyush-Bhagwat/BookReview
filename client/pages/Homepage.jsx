3490847c218000f89b8478876d631c31542f5bf7
Piyush <piyushbhagwat5@gmail.com>
2025-06-19 16:48:06 +0530

    added ignore

--- client/pages/Homepage.jsx
+++ client/pages/Homepage.jsx
@@ -12,9 +12,8 @@ const Homepage = () => {
     return (
         <div className="min-h-screen bg-gray-100 px-6 py-10">
             <div>
-            <h1 className="text-3xl font-bold text-center tracking-widest text-blue-400 mb-10">Book Explorer</h1>
+                <h1 className="text-3xl font-bold text-center tracking-widest text-blue-400 mb-10">Book Explorer</h1>
 
-            
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
